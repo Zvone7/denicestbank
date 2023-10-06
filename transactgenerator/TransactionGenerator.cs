a1ae9db915156dc30e3c4fc9445f36c0affd401d
Zvonimir Grubisic <zvonimir.grubisic@avanade.com>
2023-10-06 10:59:18 +0200

    try context loading approach

--- transactgenerator/TransactionGenerator.cs
+++ transactgenerator/TransactionGenerator.cs
@@ -6,6 +6,7 @@ using System.Net.Http.Headers;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.Identity.Client;
 using Newtonsoft.Json;
@@ -15,24 +16,40 @@ namespace transactgenerator
     public class TransactionGenerator
     {
         [FunctionName("TransactionGenerator")]
-        public async Task Run([TimerTrigger("0 * * * * *")] TimerInfo myTimer, ILogger log)
+        public async Task Run([TimerTrigger("0 * * * * *")] TimerInfo myTimer, ILogger log, ExecutionContext context)
         {
 
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
 
-            var appSettingsFilePath = "secrets.json";
-#if DEBUG
-            appSettingsFilePath = "secrets.local.json";
-#endif
+//             var appSettingsFilePath = "secrets.json";
+// #if DEBUG
+//             appSettingsFilePath = "secrets.local.json";
+// #endif
 
+            var config = new ConfigurationBuilder()
+                .SetBasePath(context.FunctionAppDirectory)
+                .AddJsonFile("secrets.json")
+                .AddJsonFile("secrets.local.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            var adSecrets = new AzureAdSecrets()
+            {
+                Instance = config["azure-instance"],
+                TenantId = config["azure-tenant-id"],
+                PortalAppId = config["portal-appreg-id"],
+                PortalDomain = config["portal-domain"],
+                TgAppId = config["transact-generator-appreg-id"],
+                TgSecret = config["transact-generator-secret"],
+            };
             // PrintAllFilesInDirectory(log);
 
-            string jsonData = File.ReadAllText(appSettingsFilePath);
-            log.LogInformation($"Found and loaded secrets.json:" + jsonData + "|");
+            // string jsonData = File.ReadAllText(appSettingsFilePath);
+            // log.LogInformation($"Found and loaded secrets.json:" + jsonData + "|");
 
-            var adSecrets = JsonConvert.DeserializeObject<AzureAdSecrets>(jsonData);
+            // var adSecrets = JsonConvert.DeserializeObject<AzureAdSecrets>(jsonData);
 
-            if (adSecrets == null) { log.LogInformation("AdSecrets are null"); }
+            if (String.IsNullOrEmpty(adSecrets.TgSecret)) { log.LogInformation("AdSecrets are null"); }
 
             log.LogInformation("*** All Aad properties loaded");
             log.LogInformation($"***_ TenantId: {adSecrets.TenantId.Substring(0, 3)}");
